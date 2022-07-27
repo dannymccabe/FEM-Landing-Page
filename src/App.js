83e93157abd9fefea8cc51e59e5d1e6965e0be27
Danny McCabe <94184743+darnomccode@users.noreply.github.com>
2022-07-27 17:30:48 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Attribution from "./components/Attribution";
+import Header from "./components/Header";
+import MainSection from "./components/MainSection";
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <MainSection />
+      <Attribution />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Attribution.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Attribution = () => {
+  return (
+    <div className="attribution">
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a href="https://dannymccabe.dev" target="_blank">
+        Danny McCabe
+      </a>
+      .
+    </div>
+  );
+};
+
+export default Attribution;
